a5df75c1e45f1b10080030cd87da2b4491ecb6cb
Fernando B <fernandobe+git@protonmail.com>
2021-03-07 13:57:24 -0600

    init

---
+++ server-dotnet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace server_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var responder = new ResponseSocket())
+            {
+                responder.Bind("tcp://*:5555");
+
+                while (true)
+                {
+                    string str = responder.ReceiveFrameString();
+                    Console.WriteLine("Received " + str);
+                    Thread.Sleep(1000);  //  Do some 'work'
+                    responder.SendFrame("World");
+                }
+            }
+        }
+    }
+}
